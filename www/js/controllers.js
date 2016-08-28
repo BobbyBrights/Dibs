c13ecc2c3fc33f6ec1fc0b6343a33f771246d2fc
Matthew Gaim <matthewgaim@gmail.com>
2016-08-28 03:13:16 -0700

    QR Code Scanner & Firebase work together to read info

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', ['ngMap','google.places','oitozero.ngSweetAlert'])
+angular.module('app.controllers', ['ngMap','google.places','oitozero.ngSweetAlert','ngCordova'])
 
 .controller('donateCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -22,28 +22,46 @@ function ($scope, $stateParams,$firebaseObject,$firebaseArray) {
 
 }])
 
-.controller('workerCtrl', ['$scope', '$stateParams','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('workerCtrl', ['$scope', '$stateParams','$state','$cordovaBarcodeScanner','$ionicPlatform', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,$firebaseObject,$firebaseArray,$state) {
+function ($scope, $stateParams,$firebaseObject,$firebaseArray, $cordovaBarcodeScanner,$ionicPlatform) {
   $scope.firebase = firebase;
 
-  $scope.createUser = function(email,password){
-    console.log(email);
-    firebase.auth().createUserWithEmailAndPassword(email,password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      if(errorCode == "auth/email-already-in-use"){
-        alert("The email is in use ");
-        $scope.sucess = false;
+  $scope.scan = function(){
+    document.addEventListener("deviceready", function () {
+      cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          $scope.splitResult = result.text.split(";");
+          $scope.name = $scope.splitResult[0];
+          $scope.id = $scope.splitResult[1];
+          alert("We got a barcode\n" +
+                "Result: " + $scope.splitResult[0] + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          "preferFrontCamera" : true, // iOS and Android
+          "showFlipCameraButton" : true, // iOS and Android
+          "prompt" : "Place a barcode inside the scan area", // supported on Android only
+          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
       }
-      // ...
-      $state.go('tabsController.workersHome', {page: 4});
-    });
-     $scope.sucess = true;
+   );
+  });
+  $scope.time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  $scope.firebase.database().ref().child("users/" + $scope.id).set({
+    name:$scope.name,
+    lastDibbed:$scope.time,
+  });
+  $scope.firebase.database().ref().child('users/' + $scope.id).on("value",function(snapshot){
+    $scope.snapshot = snapshot.val();
+  });
+  alert($scope.snapshot);
+}
 
-  }
 
 
 }])
